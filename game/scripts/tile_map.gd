f172c01fa8e9c7724551abd8cd604065059de876
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-07-20 11:55:36 -0600

    can place pieces

--- game/scripts/tile_map.gd
+++ game/scripts/tile_map.gd
@@ -1,7 +1,11 @@
 extends TileMap
 
-const DARK_GRAY = Vector2i(0,0)
-const LIGHT_GRAY = Vector2i(1,0)
+
+@onready var debounce_timer = $DebounceTimer
+
+
+const BACKGROUND_PIECE_COLOR = Vector2i(0,0)
+const FOREGROUND_PIECE_COLOR = Vector2i(1,0)
 
 # Assumes a 3x3 Grid for the shape rotation.
 # We'll want to programatically generate these later. 
@@ -18,6 +22,10 @@ const r_270 := [Vector2i(0,0), Vector2i(0,1), Vector2i(1,0), Vector2i(1,1)]
 const r := [r_0, r_90, r_180, r_270]
 
 const SHAPES := [l, r]
+
+# For debouncing layer input
+var debounce_delay = 0.1 # Time in seconds
+var can_process_input = true
 
 #grid variables
 const ROWS := 9
@@ -49,10 +57,11 @@ func move_piece(direction):
 		clear_piece()
 		current_position += direction
 		draw_piece(active_piece, current_position)
+		start_debounce()
 
 func draw_piece(piece, position):
 	for i in piece:
-		set_cell(active_layer, position + i, tile_id, LIGHT_GRAY)
+		set_cell(active_layer, position + i, tile_id, FOREGROUND_PIECE_COLOR)
 
 func is_free(position):
 	return get_cell_source_id(board_layer, position) == -1
@@ -71,7 +80,6 @@ func can_rotate():
 		if not is_free(i + current_position):
 			can_rotate = false
 	return can_rotate
-	
 
 func rotate_piece():
 	if can_rotate():
@@ -79,22 +87,34 @@ func rotate_piece():
 		current_rotation_index = (current_rotation_index + 1) % SHAPES[0].size()
 		active_piece = piece_type[current_rotation_index]
 		draw_piece(active_piece, current_position)
+		start_debounce()
 
 func clear_piece():
 	for i in active_piece:
 		erase_cell(active_layer, current_position + i)
-	
+
+func place_piece():
+	for i in active_piece:
+		erase_cell(active_layer, current_position + i)
+		set_cell(board_layer, current_position + i, tile_id, BACKGROUND_PIECE_COLOR)
+		
+	start_debounce()	
+
 func _process(delta):
-	if Input.is_action_pressed("MOVE_DOWN"):
-		move_piece(Vector2i.DOWN)
-	if Input.is_action_pressed("MOVE_UP"):
-		move_piece(Vector2i.UP)
-	if Input.is_action_pressed("MOVE_RIGHT"):
-		move_piece(Vector2i.RIGHT)
-	if Input.is_action_pressed("MOVE_LEFT"):
-		move_piece(Vector2i.LEFT)
-	if Input.is_action_pressed("ROTATE"):
-		rotate_piece()	
+	if can_process_input:
+		if Input.is_action_pressed("MOVE_DOWN"):
+			move_piece(Vector2i.DOWN)
+		if Input.is_action_pressed("MOVE_UP"):
+			move_piece(Vector2i.UP)
+		if Input.is_action_pressed("MOVE_RIGHT"):
+			move_piece(Vector2i.RIGHT)
+		if Input.is_action_pressed("MOVE_LEFT"):
+			move_piece(Vector2i.LEFT)
+		if Input.is_action_pressed("ROTATE"):
+			rotate_piece()	
+		if Input.is_action_pressed("PLACE"):
+			place_piece()
+			create_piece()
 
 func pick_piece():
 # This is where the random piece generation will go.
@@ -102,17 +122,25 @@ func pick_piece():
 
 func create_piece():
 	current_position = START_POSITION
+	piece_type = pick_piece()
 	active_piece = piece_type[current_rotation_index]
-	print('active', active_piece)
 	draw_piece(active_piece, current_position)
-	
 
-	
+func start_debounce():
+	can_process_input = false
+	debounce_timer.start(debounce_delay)
+
+
+func _on_debounce_timer_timeout():
+	print("i get calleds")
+	can_process_input = true
+
 
 func new_game():
 	piece_type = pick_piece()
-	print(piece_type)
 	create_piece()
 
 func _ready():
+
 	new_game()
+
