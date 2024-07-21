fa3a1ea58691fd652c624de7de5f25b4767643e8
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-07-20 20:29:51 -0600

    implemented better finding algo

--- game/scripts/tile_map.gd
+++ game/scripts/tile_map.gd
@@ -40,6 +40,8 @@ const r:= {
 const SHAPES := [l]
 
 # This needs rethinking
+# Target gems are drawn in a space from (0,0) -> (WIDTH, HEIGHT)
+# this allows for conversions later on assuming overlap.
 const target_gem := [Vector2i(0, 0), Vector2i(0, 1)]
 const avoid_gem := [Vector2i(0, 1), Vector2i(1, 1)]
 
@@ -200,7 +202,7 @@ func _process(delta):
 			rotate_piece()	
 		if Input.is_action_pressed("PLACE"):
 			draw_piece_on_background()
-			var result = foobar()
+			var result = find_gems()
 			print(result)
 
 
@@ -250,24 +252,59 @@ func get_next_from_queue():
 
 
 var visited = []
-func foobar():
+func find_gems():
+	var gems = []
 	visited.resize(10)
 	for i in range(10):
 		visited[i] = []
 		visited[i].resize(10)
 
-	var shapes = find_shapes()
-	print(shapes)
+	#var dark_shapes = find_shapes(BACKGROUND_PIECE_COLOR)
+	var light_shapes = find_shapes(FOREGROUND_PIECE_COLOR)
 	
+	for light_shape in light_shapes:
+		if is_target_gem(light_shape):
+			gems.append(light_shape)
+	return gems
 
-func find_shapes():
+func absolute_to_relative_shape(shape):
+	var min_x = shape.map(func(cell): return cell.x).min()
+	var min_y = shape.map(func(cell): return cell.y).min()
+	
+	return shape.map(func(cell): return Vector2i(cell.x - min_x, cell.y - min_y))
+
+
+func arrays_equal(arr1, arr2) -> bool:
+	if arr1.size() != arr2.size():
+		return false
+
+	arr1.sort()
+	arr2.sort()
+
+	for i in range(arr1.size()):
+		if arr1[i] != arr2[i]:
+			return false
+	return true
+
+
+func is_target_gem(shape):
+	if(shape.size() != target_gem.size()):
+		return false
+		
+	var relative_shape = absolute_to_relative_shape(shape)
+	
+	var are_equal = arrays_equal(relative_shape, target_gem)
+	return are_equal
+
+
+func find_shapes(desired_color: Vector2i):
 	var shapes = []
 	for x in range(10):
 		for y in range(10):
 			var color = get_cell_atlas_coords(BOARD_LAYER, Vector2i(x,y))
-			if  color != Vector2i(-1, -1) and not visited[x][y]:
+			if  color == desired_color and not visited[x][y]:
 				var shape = []
-				flood_fill(Vector2i(x, y), color, shape)
+				flood_fill(Vector2i(x, y), desired_color, shape)
 				if shape.size() > 0:
 					shapes.append(shape)
 	return shapes
