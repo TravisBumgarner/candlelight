041665d09c2fbde9b2779974cf9b48fa3439922b
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-07-23 12:17:13 -0600

    setup global UI for game

--- game/scripts/menus/main_menu.gd
+++ game/scripts/menus/main_menu.gd
@@ -4,12 +4,12 @@ extends Control
 
 func _process(_delta):
 	# Comment out the next line to show the main menu.
-	#get_tree().change_scene_to_file('res://scenes/puzzle_mode/game.tscn')
+	#get_tree().change_scene_to_file('res://scenes/mode_puzzle/puzzle_game.tscn')
 	pass
 
 
 func _on_puzzle_mode_button_up():
-	get_tree().change_scene_to_file('res://scenes/puzzle_mode/game.tscn')
+	get_tree().change_scene_to_file('res://scenes/mode_puzzle/puzzle_game.tscn')
 
 
 func _on_speed_mode_pressed():
@@ -18,6 +18,9 @@ func _on_speed_mode_pressed():
 
 func _on_options_pressed():
 	get_tree().change_scene_to_file('res://scenes/menus/options_menu.tscn')
+
+
+
 
 
 func _on_quit_pressed():
