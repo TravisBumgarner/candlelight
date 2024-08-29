36275b0192331ab5f8b761ec4c3a860910385a5d
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-08-29 16:56:02 -0600

    refactored to bae game

---
+++ game_again/GameScripts/BaseGame.gd
@@ -0,0 +1,115 @@
+extends Node2D
+
+class_name BaseGame
+
+var board_tile_map
+var target_gem_tile_map
+var queue_tile_map
+var level_complete_timer
+var sounds
+var game_details_label
+var game_details_value
+
+var history: History
+var player: Player
+var queue: Queue
+var gemsManager: GemsManager
+var level
+
+var is_paused_for_scoring = false
+
+func _init(board_tile_map, target_gem_tile_map, queue_tile_map, level_complete_timer, sounds, game_details_label, game_details_value):
+	self.board_tile_map = board_tile_map
+	self.target_gem_tile_map = target_gem_tile_map
+	self.queue_tile_map = queue_tile_map
+	self.level_complete_timer = level_complete_timer
+	self.sounds = sounds
+	self.game_details_label = game_details_label
+	self.game_details_value = game_details_value
+
+	SoundManager.connect("play_sound", sounds.play_sound)
+	InputManager.connect("action_pressed", Callable(self, "_on_action_pressed"))
+
+
+func _on_action_pressed(action):
+	print('ona ctionpressed')
+	var direction_map = {
+		"up": Vector2i.UP,
+		"down": Vector2i.DOWN,
+		"left": Vector2i.LEFT,
+		"right": Vector2i.RIGHT
+	}
+	
+	if action in direction_map:
+		player.move(direction_map[action])
+	
+	match action:
+		"undo":
+			if history.size() == 0:
+				SoundManager.play("nonmovement")
+				return
+			self.undo()
+		"rotate":
+			player.rotate_right()
+		"select":
+			history.append(board_tile_map, player)
+			player.place_on_board()
+			self.score()
+
+func score():
+	var gems = gemsManager.find_gems()
+	if(gems.size() > 0):
+		level_complete(gems)
+		return
+	player = Player.new(board_tile_map, queue.next())
+
+func level_complete(gems):
+	var total_gems = gems.size()
+	
+	if total_gems == 1:
+		SoundManager.play("one_gem")
+	if total_gems >= 2:
+		SoundManager.play("two_gems")
+			
+	for gem in gems:
+		gemsManager.draw_gem_on_board(gem)
+	is_paused_for_scoring = true
+	level_complete_timer.start(1)
+
+
+func _on_level_complete_timer_timeout():
+	level += 1
+	erase_board()
+	gemsManager.puzzle_mode_set_target_gem(level)
+	player = Player.new(board_tile_map, queue.next())
+	is_paused_for_scoring = false
+
+# This could be split into multiple functions. 
+func undo():
+	var record = history.pop()
+	if record == null:
+		return
+	self.queue.undo(player.piece_type)
+	#player.erase_piece()
+	player = record.player
+	player.draw_piece()
+	#emit_signal('experiment_undo')
+	for x in range(GlobalConsts.GRID.WIDTH):
+		for y in range(GlobalConsts.GRID.HEIGHT):
+			var tile_style = record.atlas_coords_array[x][y]
+			self.board_tile_map.erase_cell(GlobalConsts.BOARD_LAYER.PLACED_PIECES, Vector2i(x,y))
+			#if tile_style != Vector2i(-1, -1):
+			self.board_tile_map.set_cell(GlobalConsts.BOARD_LAYER.PLACED_PIECES, Vector2i(x,y), GlobalConsts.GEMS_TILE_ID, tile_style)
+
+func erase_board():
+	board_tile_map.clear_layer(GlobalConsts.BOARD_LAYER.PLACED_PIECES)
+	board_tile_map.clear_layer(GlobalConsts.BOARD_LAYER.CURRENT_PIECE)
+
+func new_game():
+	level = 1
+	erase_board()
+	history = History.new()
+	queue = Queue.new(queue_tile_map, null)
+	player = Player.new(board_tile_map, queue.next())
+	gemsManager = GemsManager.new(board_tile_map, target_gem_tile_map, queue_tile_map)
+	gemsManager.puzzle_mode_set_target_gem(level)
