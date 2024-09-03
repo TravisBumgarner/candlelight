684a4a00b538415c0f274a035fe05048b2b83d8a
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-09-03 15:11:01 -0600

    Scale Game (#44)

--- game_again/Singletons/Utilities.gd
+++ game_again/Singletons/Utilities.gd
@@ -1,16 +1,7 @@
 extends Node
 
-#static func is_cell_empty(tile_map, cell):
-	#return tile_map.get_cell_source_id(GlobalConsts.BOARD_LAYER.PLACED_PIECES, cell) == -1
-
 static func is_cell_border(tile_map, cell):
 	return not(tile_map.get_cell_source_id(GlobalConsts.BOARD_LAYER.BORDER, cell) == -1)
-
-
-#static func erase_area(tile_map, start: Vector2i, end: Vector2i, layer: int):
-	#for x in range(start.x, end.x):
-		#for y in range(start.y, end.y):
-			#tile_map.erase_cell(layer, Vector2i(x,y))
 
 func swap(i : int, j : int, a : Array) -> Array:
 	var t = a[i]
