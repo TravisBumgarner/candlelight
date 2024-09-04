5c1dde77d8eb6a15f88ac80399a4fd62bde4c852
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-09-04 15:09:18 -0600

    added routing for create challenges

--- game_again/MainMenu/index.gd
+++ game_again/MainMenu/index.gd
@@ -4,30 +4,36 @@ class_name MainMenu
 @onready var daily_game_button = $Daily/DailyGame
 @onready var puzzle_game_button = $PuzzleGame/PuzzleGame
 @onready var apprenticeship_game_button = $ApprenticeshipGame/ApprenticeshipGame
+@onready var play_challenge_button = $PlayerGenerated/PlayChallenge
+@onready var create_challenge_button = $PlayerGenerated/CreateChallenge
 
 @onready var exit = $Exit/Exit
 
-@onready var game = preload("res://game.tscn")
+@onready var game_scene = preload("res://game.tscn")
+@onready var create_challenge_scene = preload("res://CreateChallenge/index.tscn")
 
 
 func _ready():
 	daily_game_button.connect("pressed", Callable(self, "on_daily_game_button_down"))
 	puzzle_game_button.connect("pressed", Callable(self, "on_puzzle_game_button_down"))
 	apprenticeship_game_button.connect("pressed", Callable(self, "on_apprenticeship_game_button_down"))
+	create_challenge_button.connect("pressed", Callable(self, "on_create_challenge_button_down"))
 	exit.connect("pressed", Callable(self, "on_exit_button_down"))
 
 func on_apprenticeship_game_button_down():
 	GlobalState.gameMode = GlobalConsts.GAME_MODE.ApprenticeshipGame
-	get_tree().change_scene_to_packed(game)
+	get_tree().change_scene_to_packed(game_scene)
 	
-
 func on_daily_game_button_down():
 	GlobalState.gameMode = GlobalConsts.GAME_MODE.DailyGame
-	get_tree().change_scene_to_packed(game)
+	get_tree().change_scene_to_packed(game_scene)
 	
 func on_puzzle_game_button_down():
 	GlobalState.gameMode = GlobalConsts.GAME_MODE.PuzzleGame
-	get_tree().change_scene_to_packed(game)
-	
+	get_tree().change_scene_to_packed(game_scene)
+
+func on_create_challenge_button_down():
+	get_tree().change_scene_to_packed(create_challenge_scene)
+
 func on_exit_button_down():
 	get_tree().quit()
