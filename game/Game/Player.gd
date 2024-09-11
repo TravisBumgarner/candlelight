99a68ee242d91384d888bf728a4adc654c670eb3
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-09-11 16:40:46 -0600

    current wip

---
+++ game/Game/Player.gd
@@ -0,0 +1,103 @@
+extends Node2D
+class_name Player
+
+var board_tile_map: TileMap
+var piece_type
+var rotation_index
+var current_absolute_position
+
+
+func _init(_board_tile_map, _piece_type):
+	self.board_tile_map = _board_tile_map
+	self.piece_type = _piece_type
+	self.rotation_index = 0
+	self.current_absolute_position = GlobalConsts.STARTING_SPACE_ORIGIN
+	self.draw_piece()
+	
+func move(direction):
+	if can_move(direction):
+		SoundManager.play("movement")
+		self.current_absolute_position += direction
+		self.draw_piece()
+	else:
+		SoundManager.play("nonmovement")
+		pass
+		
+
+func get_current_piece_rotation():
+	return self.piece_type[self.rotation_index]
+
+func draw_piece():
+	self.board_tile_map.clear_layer(GlobalConsts.BOARD_LAYER.CURRENT_PIECE)
+	for relative_position in self.get_current_piece_rotation():
+		var placed_tile = self.board_tile_map.get_cell_atlas_coords(GlobalConsts.BOARD_LAYER.PLACED_PIECES, self.current_absolute_position + relative_position)
+		var tile_style: Vector2i
+		
+		if(placed_tile == Vector2i(-1,-1)):
+			tile_style = GlobalConsts.SPRITE.DARK_ACTIVE
+		elif(placed_tile == GlobalConsts.SPRITE.DARK_INACTIVE):
+			tile_style = GlobalConsts.SPRITE.LIGHT_ACTIVE
+		elif(placed_tile == GlobalConsts.SPRITE.LIGHT_INACTIVE):
+			tile_style = GlobalConsts.SPRITE.DARK_ACTIVE
+	
+		self.board_tile_map.set_cell(GlobalConsts.BOARD_LAYER.CURRENT_PIECE, self.current_absolute_position + relative_position, GlobalConsts.GEMS_TILE_ID, tile_style)
+
+
+func can_move(direction):
+	for point in self.get_current_piece_rotation():
+		var is_cell_border = Utilities.is_cell_border(self.board_tile_map, point + self.current_absolute_position + direction)
+		if is_cell_border:
+			return false
+	return true
+	
+
+func can_place():
+	for point in self.get_current_piece_rotation():
+		var current_point = point + self.current_absolute_position
+		var is_cell_border = Utilities.is_cell_border(self.board_tile_map, current_point)
+		if is_cell_border:
+			return false
+
+		var is_in_starting_area = current_point[1] < 0
+		if is_in_starting_area:
+			return false
+		
+		var is_cell_gem  = Utilities.is_cell_blocker(self.board_tile_map, current_point)
+		if is_cell_gem:
+			return false
+	return true
+
+
+func can_rotate():
+	var temporary_rotation_index = (self.rotation_index + 1) % Shapes.TOTAL_ROTATIONS
+	for point in piece_type[temporary_rotation_index]:
+		var is_cell_border = Utilities.is_cell_border(self.board_tile_map, point + current_absolute_position)
+		if is_cell_border:
+			return false
+	return true
+
+
+func rotate_right():
+	if self.can_rotate():
+		SoundManager.play("movement")
+		self.rotation_index = (self.rotation_index + 1) % Shapes.TOTAL_ROTATIONS
+		self.draw_piece()
+	else:
+		SoundManager.play("nonmovement")
+		pass
+
+func place_on_board():
+	self.board_tile_map.clear_layer(GlobalConsts.BOARD_LAYER.CURRENT_PIECE)
+	for relative_position in self.get_current_piece_rotation():
+		var placed_tile = self.board_tile_map.get_cell_atlas_coords(GlobalConsts.BOARD_LAYER.PLACED_PIECES, self.current_absolute_position + relative_position)
+		var tile_style: Vector2i
+		
+		if(placed_tile == GlobalConsts.SPRITE.DARK_INACTIVE):
+			tile_style = GlobalConsts.SPRITE.LIGHT_INACTIVE
+		elif(placed_tile == GlobalConsts.SPRITE.LIGHT_INACTIVE):
+			tile_style = GlobalConsts.SPRITE.DARK_INACTIVE
+		else:
+			# Handles both null and (-1, -1) for tile. Depending on undo.
+			# There's something off here I don't fully understand.
+			tile_style = GlobalConsts.SPRITE.DARK_INACTIVE
+		self.board_tile_map.set_cell(GlobalConsts.BOARD_LAYER.PLACED_PIECES, current_absolute_position + relative_position, GlobalConsts.GEMS_TILE_ID, tile_style)
---
+++ game/Game/PuzzleGameMenu.gd
@@ -0,0 +1,78 @@
+extends Control
+
+@onready var save_buttons_container = $SaveButtonsContainer
+@onready var new_game_button = $TextureRect/NewGame
+@onready var game_scene = load("res://Game/game_board.tscn")
+
+
+
+var save_files = []  # Store the list of available saves
+
+# Called when the node enters the scene tree
+func _ready():
+	# Scan for save files in the user:// directory
+	check_for_saves()
+
+# Function to check for save files and create buttons dynamically
+func check_for_saves():
+	var game_save_dir = Utilities.get_save_game_dir(GlobalConsts.GAME_SAVE_KEYS.PUZZLE_GAME)
+	var dir = DirAccess.open(game_save_dir)
+	
+	if dir == null:
+		print("Error: Unable to access the save directory.")
+		return
+	
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+	var full_file_path = "%s/%s" % [game_save_dir, file_name]
+
+	
+	while full_file_path != "":
+		if full_file_path.ends_with(".save"):
+			# Found a save file
+			save_files.append(full_file_path)
+			create_save_button(full_file_path)
+		full_file_path = dir.get_next()
+	
+	dir.list_dir_end()
+
+# Function to create a button for each save file
+func create_save_button(file_name: String):
+	var button = Button.new()
+	button.text = "Play " + file_name
+	button.name = file_name
+	button.connect("pressed", Callable(self, "_on_save_button_pressed").bind(file_name))
+	
+	# Add the button to a container (e.g., a VBoxContainer or Panel)
+	save_buttons_container.add_child(button)
+
+# Function to handle button press, loading the save file
+func _on_save_button_pressed(file_name: String):
+	print("Loading save:", file_name)
+	load_game(file_name)
+
+# Function to load the game based on the selected save file
+func load_game(file_name: String):
+	if not FileAccess.file_exists(file_name):
+		print("Error: Save file not found.")
+		return
+	
+	var file = FileAccess.open(file_name, FileAccess.READ)
+	var save_data = file.get_as_text()  # Assuming the save is in a readable format (e.g., JSON)
+	file.close()
+	
+		# Create an instance of the JSON class and parse the text
+	var parse_result = JSON.parse_string(save_data)
+	
+	if not parse_result is Dictionary:
+		print("Failed to parse saved game data")
+		return null
+
+	GlobalState.game_mode = GlobalConsts.GAME_MODE.PuzzleGame
+	GlobalState.game_save_data = parse_result
+	get_tree().change_scene_to_packed(game_scene)	
+
+
+func _on_new_game_pressed():
+	GlobalState.game_mode = GlobalConsts.GAME_MODE.PuzzleGame
+	get_tree().change_scene_to_packed(game_scene)
