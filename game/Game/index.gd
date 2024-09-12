1713c1ea56fce5ad63a024d27263625fcf421099
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-09-12 17:03:34 -0600

    refactor of init

--- game/Game/index.gd
+++ game/Game/index.gd
@@ -9,6 +9,7 @@ extends Node2D
 @onready var game_details_value = $GameDetailsTileMap/Control/VBoxContainer/GameDetailsValue
 @onready var instructions = $Instructions
 @onready var game_details_tile_map = $GameDetailsTileMap
+@onready var submit_score = $SubmitScore
 
 const main_menu_scene = preload("res://MainMenu/main_menu.tscn")
 
@@ -17,55 +18,41 @@ func return_to_main_menu():
 
 var game 
 
+func create_game(game_mode, show_submit_score: bool) -> void:
+	game = game_mode.new([
+		# ALPHABETICAL
+		board_tile_map, 
+		game_details_label, 
+		game_details_tile_map,
+		game_details_value,
+		instructions,
+		level_complete_timer, 
+		queue_tile_map, 
+		Callable(self, "return_to_main_menu"),
+		sounds, 
+		target_gem_tile_map
+		# ALPHABETICAL
+	])
+	if show_submit_score:
+		submit_score.show()
+	else:
+		submit_score.hide()
+
 func _ready():
-	if GlobalState.game_mode == GlobalConsts.GAME_MODE.ApprenticeshipGame:
-		game = ApprenticeshipGame.new(
-		board_tile_map, 
-		target_gem_tile_map, 
-		queue_tile_map, 
-		level_complete_timer, 
-		sounds, 
-		game_details_label, 
-		game_details_value,
-		game_details_tile_map,
-		instructions,
-		Callable(self, "return_to_main_menu")
-	)
+	var game_modes = {
+		GlobalConsts.GAME_MODE.ApprenticeshipGame: { "class": ApprenticeshipGame, "show_score": false },
+		GlobalConsts.GAME_MODE.PuzzleGame: { "class": PuzzleGame, "show_score": true },
+		GlobalConsts.GAME_MODE.DailyGame: { "class": DailyGame, "show_score": false }
+	}
 	
-	if GlobalState.game_mode == GlobalConsts.GAME_MODE.PuzzleGame:
-		game = PuzzleGame.new(
-		board_tile_map, 
-		target_gem_tile_map, 
-		queue_tile_map, 
-		level_complete_timer, 
-		sounds, 
-		game_details_label, 
-		game_details_value,
-		game_details_tile_map,
-		instructions,
-		Callable(self, "return_to_main_menu")
-	)
-	
-	if GlobalState.game_mode == GlobalConsts.GAME_MODE.DailyGame:
-		game = DailyGame.new(
-		board_tile_map, 
-		target_gem_tile_map, 
-		queue_tile_map, 
-		level_complete_timer, 
-		sounds, 
-		game_details_label, 
-		game_details_value,
-		game_details_tile_map,
-		instructions,
-		Callable(self, "return_to_main_menu")
-	)
-	
+	if GlobalState.game_mode in game_modes:
+		var selected_game = game_modes[GlobalState.game_mode]
+		create_game(selected_game["class"], selected_game["show_score"])
+		
 	if GlobalState.game_save_file:
 		game.load_game()
 		GlobalState.game_save_file = null
 	else:
 		game.new_game()
-	
-	
-	
+
 
