e2a41aa55c2243ce73f4169c1888a2704458e718
Travis Bumgarner <travis.bumgarner@gmail.com>
2025-01-23 20:23:41 -0600

    implemented

---
+++ website/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+LOCAL_DIR="build/"
+REMOTE_HOST="nfs_candlelight" # This value comes from ~/.ssh/config
+REMOTE_DIR="/home/public"
+
+rsync -avz --delete "$LOCAL_DIR" "$REMOTE_HOST:$REMOTE_DIR"
+echo "Files uploaded successfully to $REMOTE_HOST:$REMOTE_DIR"
