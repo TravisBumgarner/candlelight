36e939324d459553a748f315892ceb12a89689f0
Travis Bumgarner <travis.bumgarner@gmail.com>
2025-02-10 16:13:48 -0600

    Nearly Free Speech Migration (And By Accident, Getting back into developing game) (#75)
    * implemented
    
    * fixed level building setup

--- game/Tutorial/tutorial_menu.gd
+++ game/Tutorial/tutorial_menu.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @onready var main_menu = load("res://MainMenu/main_menu.tscn")
-@onready var game_scene = load("res://Game/game_board.tscn")
+@onready var game_scene = load("res://GameBase/game_board.tscn")
 
 func _ready():
 	InputManager.connect("action_pressed", Callable(self, "_on_action_pressed"))
@@ -12,5 +12,6 @@ func _on_action_pressed(action):
 		"escape":
 			get_tree().change_scene_to_packed(main_menu)
 		"select":
+			print('select')
 			GlobalState.game_mode = GlobalConsts.GAME_MODE.Tutorial
 			get_tree().change_scene_to_packed(game_scene)
